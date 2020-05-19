01f03d48872a64eb5163d4d5a83f818a5f07dfc0
RR-inc <53051377+RR-inc@users.noreply.github.com>
2020-05-19 19:37:55 +0200

    Create init.js

---
+++ examples/js/init.js
@@ -0,0 +1,21 @@
+if(!Done)Done={status:false};
+
+var loadedDo=function(){
+  if(!Done.status){
+    Done.status=true;
+			var bott=document.createElement('div');
+      bott.classList.add("bottom");
+      
+      
+			var buttSource=document.createElement('div');
+      buttSource.classList.add("button");
+      buttSource.classList.add("source");
+      var pageName=''+window.location.pathname;
+      buttSource.innerHTML='<a title="source" href="https://github.com/RR-inc/RR-inc.github.io/edit/master' + pageName + '" target="blank">Source</a>';
+      
+			bott.body.appendChild(buttSource);
+      
+			document.body.appendChild(bott);
+  }
+};
+	document.addEventListener('DOMContentLoaded', loadedDo);
